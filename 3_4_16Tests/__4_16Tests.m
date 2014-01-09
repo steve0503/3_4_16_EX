7cf5b19ed32e0d11d1124f712da4d40b97308e16
steve0503 <leehr76@gmail.com>
2014-01-09 11:19:12 +0900

    Initial Commit

---
+++ 3_4_16Tests/__4_16Tests.m
@@ -0,0 +1,34 @@
+//
+//  __4_16Tests.m
+//  3_4_16Tests
+//
+//  Created by SDT-1 on 2014. 1. 9..
+//  Copyright (c) 2014년 steve. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __4_16Tests : XCTestCase
+
+@end
+
+@implementation __4_16Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
